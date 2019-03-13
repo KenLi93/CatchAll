4129e9a9c24e74337f3e112b2c2f2ad6c1ee3d1a
Qijun Li <qijunl2@u.washington.edu>
2019-03-13 14:33:24 -0700

    more tests

--- tests/testthat/test_best_models.R
+++ tests/testthat/test_best_models.R
@@ -13,4 +13,8 @@ test_that("Analysis of the apples dataset gives the same best model", {
   expect_equal(apple_best_model$other$cutoff,
                163)
 
+  ## the estimate doesn't differ much
+  expect_lte(abs(apple_best_model$est - 1477.1), 0.05 * 1477.1)
+
+
 })
